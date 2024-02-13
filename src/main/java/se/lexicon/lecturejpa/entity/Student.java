3d82533587bcdeb27df05fa0a28922537c08c383
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-02-13 10:39:53 +0100

    Lecture - JPA - Student Entity - Created DB Table with ORM
    (cherry picked from commit a68caec4a9703a6de37013e353134c007ff9bce1)

---
+++ src/main/java/se/lexicon/lecturejpa/entity/Student.java
@@ -0,0 +1,41 @@
+package se.lexicon.lecturejpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+//@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+//@Entity(name = "TBL_STUDENTS")
+@Entity
+public class Student {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // For id number generations
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator") // TODO - Deprecated 6.2?
+    private String id;
+
+    @Setter
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Setter
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Setter
+    @Column(nullable = false, unique = true)
+    private String email;
+
+//    @Column(name = "_STATUS") // Custom Name in Table
+    private boolean status;
+    private LocalDateTime createDate;
+}
