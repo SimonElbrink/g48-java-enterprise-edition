0240188e8cfd7ffd5eb681ce4337df331cb74ae2
Negar Baharmand <negarbaharmand@outlook.com>
2024-03-01 14:22:30 +0100

    Define methods in TaskRepository
    (cherry picked from commit 0ef33be0e7f4883f510e025239c1882cf0941583)

--- src/main/java/se/lexicon/g48todoapi/repository/TaskRepository.java
+++ src/main/java/se/lexicon/g48todoapi/repository/TaskRepository.java
@@ -1,18 +1,40 @@
 package se.lexicon.g48todoapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.lexicon.g48todoapi.domain.entity.Task;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     //todo: select tasks contain title
+    List<Task> findByTitleContains(String title);
+
     //todo: select tasks by person id
+    List<Task> findByPerson_Id(Long personId);
+
     //todo: select tasks by status
-    //todo: select tasks by date between start and end
+    List<Task> findByDone(boolean done);
+
+    //todo: select tasks by date between start and end(two ways)
+    @Query("select t from Task t where t.deadline between :from and :to")
+    List<Task> selectTasksBetweenDates(@Param("from") LocalDate from, @Param("to") LocalDate to);
+    List<Task> findByDeadlineBetween(LocalDate from, LocalDate to);
+
     //todo: select all unassigned tasks
+    List<Task> findByPersonIsNull();
+
     //todo: select all unfinished tasks
+    @Query("select t from Task t where t.done = false")
+    List<Task> selectUnFinishedTasks();
+
     //todo: select all unfinished and overdue tasks
+    @Query("select t from Task t where t.done = false and current_date > t.deadline ")
+    List<Task> selectUnFinishedAndOverdueTasks();
 
 }
